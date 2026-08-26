35d0cb8ed79551d43b744e226c0ac4074f59ef3f
GraphicInitalyRD <graphics@initalyrd.com>
2026-08-26 10:15:21 +0200

    Add files via upload

--- site.js
+++ site.js
@@ -81,7 +81,7 @@
         '</div>' +
         menuLink('color-chart','color-chart.html','Color Chart',active) +
         menuLink('brochures','brochures.html','Brochures',active) +
-        menuLink('sds','#','SDS Files',active) +
+        menuLink('sds','sds-files.html','SDS Files',active) +
         menuLink('distributors','distributors.html','Distributors',active) +
       '</nav>';
   }
